57d468c7ab42764238d521d3fa749b0b01317f8d
Alex <revooms@users.noreply.github.com>
2025-07-26 12:46:50 +0200

    Add addons

---
+++ addons/brombeer.Galant/autoload/game.gd
@@ -0,0 +1,26 @@
+extends Node
+class_name Game
+
+var title: String = "Untitled Game"
+var version: String = "0.1"
+var author: String = "Unnamed Author"
+var release_date: String = "2025-01-01" # Y-m-d
+
+var is_ready: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	setIsReady(true)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func getPlayer() -> void:
+	pass
+
+func setIsReady(state: bool) -> void:
+	is_ready = state
+
+	if state == true:
+		print("Game is ready")
---
+++ addons/brombeer.Galant/galant.gd
@@ -0,0 +1,36 @@
+@tool
+extends EditorPlugin
+
+# Replace this value with a PascalCase autoload name, as per the GDScript style guide.
+const ROOT_PATH = "res://addons/brombeer.Galant"
+const AUTOLOAD_NAME_GAME = "BrombeerGame"
+const AUTOLOAD_NAME_EVENTHUB = "BrombeerEventHub"
+
+var autoloads = [
+	{
+		"name": "BrombeerGame",
+		"autoload": "res://addons/brombeer.Galant/autoload/game.gd",
+	},
+	{
+		"name": "BrombeerEventHub",
+		"autoload": "res://addons/brombeer.Galant/autoload/eventhub.gd",
+	},
+]
+
+func _enter_tree() -> void:
+	# Initialization of the plugin goes here.
+	pass
+
+func _exit_tree() -> void:
+	# Clean-up of the plugin goes here.
+	pass
+
+func _enable_plugin():
+	# The autoload can be a scene or script file.
+	for i in autoloads:
+		add_autoload_singleton(i.name, i.autoload)
+
+func _disable_plugin():
+	for i in autoloads:
+		remove_autoload_singleton(i.name)
+
