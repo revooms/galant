28e72ad6a14efc9cbe6b018ce7278cdcaace417c
Alex <revooms@users.noreply.github.com>
2025-07-27 16:47:32 +0200

    Update galant_node2d_rotator.gd

--- addons/revooms.Galant/resources/galant_node2d_rotator.gd
+++ addons/revooms.Galant/resources/galant_node2d_rotator.gd
@@ -4,15 +4,6 @@ class_name GalantNode2DRotator
 @export_range(0.01, 0.2) var speed: float = 0.1
 @export var rotate_clockwise: bool = true
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("Node2DRotator ready")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
 func _physics_process(delta: float) -> void:
 	if Engine.is_editor_hint():
 		return
